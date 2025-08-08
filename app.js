8ddc2cf32245a6f998abd98526339ab5b8aea45f
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2025-08-08 09:55:16 -0300

    Adicionando busca de imagens por numero e funcionalidade no botão de rodar automaticamente

--- app.js
+++ app.js
@@ -4,8 +4,34 @@ const botaoTrocarImagem = document.getElementById('trocar-imagem')
 
 function trocarImagem(){
     const image = document.getElementById('image').value
-    const bgImage = '\"./img/'+ image + '.jpeg\"'
-    document.documentElement.style.setProperty('--bg-image', bgImage)
+    const imagemSelecionada = image.replace(' ','-')
+
+
+    document.documentElement.style.setProperty( '--bg-image', `var(--${imagemSelecionada})` )
 } 
 
 botaoTrocarImagem.addEventListener('click' , trocarImagem)
+
+const imagemContainer = document.querySelector('.imagem');
+const botaoAutomatico = document.getElementById('automatico');
+
+let animacaoOn = false
+
+function modoAutomatico(){
+
+    //inverte o estado da variavel animacaoOn
+    animacaoOn = !animacaoOn
+
+    if (animacaoOn) {
+        // Se for para rodar, define a propriedade 'animation' diretamente no elemento
+        imagemContainer.style.animation = 'slider-automatico 27s infinite linear';
+        botaoAutomatico.textContent = 'Parar Modo Automático';
+    } else {
+        // Se for para parar, limpa a propriedade 'animation'
+        imagemContainer.style.animation = 'none';
+        botaoAutomatico.textContent = 'Iniciar Modo Automático';
+    }
+}
+
+
+botaoAutomatico.addEventListener('click', modoAutomatico);
